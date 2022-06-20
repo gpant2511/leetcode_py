00476a4f10991b3185eb3bc841b626f8aaa14ddf
gpant2511 <g.pant2511@gmail.com>
2022-06-20 11:57:49 -0500

    Solution to Leetcode problem #1523

---
+++ lc_1523.py
@@ -0,0 +1,18 @@
+### LC Problem
+### Difficulty Easy
+### 1523. Count Odd Numbers in an Interval Range
+### https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        a=high-low+1
+        if(low%2!=0 and high%2!=0):
+            b=a//2+1
+        else:
+            b=a//2
+        
+        return(b)
+
+solution = Solution()
+print(solution.countOdds(3, 7))
+print(solution.countOdds(8, 10))
