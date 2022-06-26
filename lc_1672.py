08a982313336673a0474c1dcebdbda5162f91f4a
gpant2511 <g.pant2511@gmail.com>
2022-06-26 12:08:01 -0500

    Leetcode problem with unit test

--- lc_1672.py
+++ lc_1672.py
@@ -4,8 +4,12 @@
 ### https://leetcode.com/problems/richest-customer-wealth/
 
 from typing import List
+import unittest
 
 class Solution:
+    def maximumWealth_optimal(self, accounts: List[List[int]]) -> int:
+        return 0
+    
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         x = len(accounts)
         z = 0
@@ -17,6 +21,18 @@ class Solution:
             z=0
         return max(sumlist)
 
-accounts = [[1,2,3],[3,2,1]]
-solution = Solution()
-print(solution.maximumWealth(accounts))
+class TestSolution(unittest.TestCase):
+
+    def setUp(self):
+        self.solution = Solution()
+    
+    def test_maximumWealth_1(self):
+        accounts = [[1,2,3],[3,2,1]]
+        self.assertEqual(self.solution.maximumWealth(accounts), 6)
+
+    def test_maximumWealth_2(self):
+        accounts = [[1,5],[7,3],[3,5]]
+        self.assertEqual(self.solution.maximumWealth(accounts), 10)
+    
+if __name__ == '__main__':
+    unittest.main()
